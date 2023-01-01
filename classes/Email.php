b74bd0a329912aca2641cf1cbc1f18bf48e40777
Edgar Perez <edgarjoel9912@gmail.com>
2023-01-01 16:35:12 -0400

    Deplyment de la app

---
+++ classes/Email.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Classes;
+
+use PHPMailer\PHPMailer\PHPMailer;
+
+class Email {
+
+    public $email;
+    public $nombre;
+    public $token;
+
+    public function __construct($email, $nombre, $token) {
+        $this->email = $email;
+        $this->nombre = $nombre;
+        $this->token = $token;
+    }
+
+    public function enviarConfirmacion() {
+
+        //Crear el objeto de email
+        $mail = new PHPMailer();
+        $mail->isSMTP();
+        $mail->Host = 'smtp.mailtrap.io';
+        $mail->SMTPAuth = true;
+        $mail->Port = 2525;
+        $mail->Username = '6e5651fb4a2c6f';
+        $mail->Password = '30dabb62c0fd66';
+
+        $mail->setFrom('admin@bienesraices.com');
+        $mail->addAddress('admin@bienesraices.com', 'BienesRaices.com');
+        $mail->Subject = 'Confirma tu cuenta';
+
+        //Set HTML
+        $mail->isHTML(TRUE);
+        $mail->CharSet = 'UTF-8';
+
+        $contenido = "<html>";
+        $contenido .= "<p><strong>Hola " . $this->nombre . "</strong> Has creado tu cuenta en App Salon, ahora debes confirmarla presionando el siguiente enlace</p>";
+        $contenido .= "<p>Presiona aqui: <a href='http://localhost:3000/confirmar-cuenta?token=" . $this->token . "'>Confirmar Cuenta</a></p>";
+        $contenido .= "<p>Si no has sido tu quien solicito esta cuenta, puedes ignorar el mensaje</p>";
+        $contenido .= "</html>";
+        $mail->Body = $contenido;
+
+        //Enviar el email
+        $mail->send();
+    }
+
+    public function enviarInstrucciones() {
+        //Crear el objeto de email
+        $mail = new PHPMailer();
+        $mail->isSMTP();
+        $mail->Host = 'smtp.mailtrap.io';
+        $mail->SMTPAuth = true;
+        $mail->Port = 2525;
+        $mail->Username = '6e5651fb4a2c6f';
+        $mail->Password = '30dabb62c0fd66';
+
+        $mail->setFrom('admin@bienesraices.com');
+        $mail->addAddress('admin@bienesraices.com', 'BienesRaices.com');
+        $mail->Subject = 'Reestablece tu password';
+
+        //Set HTML
+        $mail->isHTML(TRUE);
+        $mail->CharSet = 'UTF-8';
+
+        $contenido = "<html>";
+        $contenido .= "<p><strong>Hola " . $this->nombre . "</strong> Has solicitado reestablecer tu password, sigue el siguiente enlace para hacerlo.</p>";
+        $contenido .= "<p>Presiona aqui: <a href='http://localhost:3000/recuperar?token=" . $this->token . "'>Reestablecer Password</a></p>";
+        $contenido .= "<p>Si no has sido tu quien solicito este cambio, puedes ignorar el mensaje</p>";
+        $contenido .= "</html>";
+        $mail->Body = $contenido;
+
+        //Enviar el email
+        $mail->send();
+    }
+}
