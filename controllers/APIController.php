270373e9c09eb171defda1b606c40a84ec75aafd
Edgar Perez <edgarjoel9912@gmail.com>
2023-01-05 19:36:16 -0400

    Refactoring

--- controllers/APIController.php
+++ controllers/APIController.php
@@ -9,9 +9,7 @@ use Model\Servicio;
 class APIController {
     public static function index() {
         $servicios = Servicio::all();
-
-        debuguear($servicios);
-        // echo json_encode($servicios);
+        echo json_encode($servicios);
     }
 
     // public static function guardar() {
