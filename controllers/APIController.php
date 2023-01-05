6695e79e552bd6c2f20a7e5108b70273edfe39bb
Edgar Perez <edgarjoel9912@gmail.com>
2023-01-05 19:28:42 -0400

    Refactoring

--- controllers/APIController.php
+++ controllers/APIController.php
@@ -8,40 +8,41 @@ use Model\Servicio;
 
 class APIController {
     public static function index() {
-        echo "desde api/index";
-        // $servicios = Servicio::all();
+        $servicios = Servicio::all();
+
+        debuguear($servicios);
         // echo json_encode($servicios);
     }
 
-    public static function guardar() {
+    // public static function guardar() {
 
-        //Almacena la Cita y devuelve el ID
-        $cita = new Cita($_POST);
-        $resultado = $cita->guardar();
+    //     //Almacena la Cita y devuelve el ID
+    //     $cita = new Cita($_POST);
+    //     $resultado = $cita->guardar();
 
-        $id = $resultado['id'];
+    //     $id = $resultado['id'];
 
-        //Almacena los servicios con el id de la cita
-        $idServicios = explode(",", $_POST['servicios']);
-        foreach ($idServicios as $idServicio) {
-            $args = [
-                'citaId' => $id,
-                'servicioId' => $idServicio
-            ];
-            $citaServicio = new CitaServicio($args);
-            $citaServicio->guardar();
-        }
+    //     //Almacena los servicios con el id de la cita
+    //     $idServicios = explode(",", $_POST['servicios']);
+    //     foreach ($idServicios as $idServicio) {
+    //         $args = [
+    //             'citaId' => $id,
+    //             'servicioId' => $idServicio
+    //         ];
+    //         $citaServicio = new CitaServicio($args);
+    //         $citaServicio->guardar();
+    //     }
 
-        echo json_encode(['resultado' => $resultado]);
-    }
+    //     echo json_encode(['resultado' => $resultado]);
+    // }
 
-    public static function eliminar() {
+    // public static function eliminar() {
         
-        if($_SERVER['REQUEST_METHOD'] === 'POST') {
-            $id = $_POST['id'];
-            $cita = Cita::find($id);
-            $cita->eliminar();
-            header('Location:' . $_SERVER['HTTP_REFERER']);
-        }
-    }
+    //     if($_SERVER['REQUEST_METHOD'] === 'POST') {
+    //         $id = $_POST['id'];
+    //         $cita = Cita::find($id);
+    //         $cita->eliminar();
+    //         header('Location:' . $_SERVER['HTTP_REFERER']);
+    //     }
+    // }
 }
